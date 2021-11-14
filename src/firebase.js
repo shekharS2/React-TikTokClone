1b5d8691f8e91053856353ddfa29009a38e71d38
shekharS2 <saikiashekhar25@gmail.com>
2021-11-14 13:57:47 +0530

    second

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,26 @@
-import firebase from "firebase/app";
-import config from "./config.json";
-import "firebase/firestore";
-import "firebase/auth";
-import "firebase/storage";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
 
-firebase.initializeApp(config);
+const firebaseConfig = {
+  apiKey: "AIzaSyAVO92CRJ36Vsf9M-Gft2RnOlUhjf4eDOw",
+  authDomain: "tiktokclone-react.firebaseapp.com",
+  projectId: "tiktokclone-react",
+  storageBucket: "tiktokclone-react.appspot.com",
+  messagingSenderId: "231375711009",
+  appId: "1:231375711009:web:c755dd41adcd71cd716c60"
+};
 
-let provider = new firebase.auth.GoogleAuthProvider();
+firebase.initializeApp(firebaseConfig);
+
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+
+let provider = new firebase.auth.GoogleAuthProvider();
 
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
